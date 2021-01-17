312de5568c355e042274acf9e92a8bcc37778274
Knut Lorenzen <lo.knut@gmail.com>
2021-01-17 14:20:05 +0000

    initial commit

---
+++ Birthdays/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Birthdays
+//
+//  Created by Knut Lorenzen on 16/01/2021.
+//  Copyright © 2021 anonoymous. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
